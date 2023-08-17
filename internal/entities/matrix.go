6b6c23794b995fca6d2eccfb8b386da11c74af0a
dbtrnl <daniel.bittar@alumni.usp.br>
2023-08-17 02:31:01 -0300

    feat(entities): create 'matrix' entity
    plus unit testing of all main funcs of the entity.
    entity will be used first in problem 11.

--- internal/entities/matrix.go
+++ internal/entities/matrix.go
@@ -1,9 +1,28 @@
 package entities
 
+import (
+	"errors"
+	"fmt"
+)
+
+type rowOrCol string
+
+const (
+	row rowOrCol = "row"
+	col rowOrCol = "col"
+)
+
+type leftOrRight string
+
+const (
+	left  leftOrRight = "left"
+	right leftOrRight = "right"
+)
+
 type Matrix [][]int
 
 // Returns a "rows x cols" matrix filled with zeroes.
-func (m *Matrix) NewMatrix(rows, cols int) {
+func (m *Matrix) ResizeMatrix(rows, cols int) {
 	matrix := make([][]int, rows)
 	for i := 0; i < rows; i++ {
 		matrix[i] = make([]int, cols)
@@ -19,7 +38,7 @@ func (m *Matrix) AssignValues(data []int) {
 	for r := 0; r < rows; r++ {
 		for c := 0; c < cols; c++ {
 			if currNum == dataLen {
-				break // 10x10 matrix requires 100 numbers, if there are less, loops stops to avoid "index out of range" err
+				break // 10x10 matrix requires 100 numbers, if there are less, loop stops to avoid error
 			}
 			(*m)[r][c] = data[currNum]
 			currNum++
@@ -27,17 +46,108 @@ func (m *Matrix) AssignValues(data []int) {
 	}
 }
 
-/* This method is not necessary, since NewMatrix assures it will be valid
-func (m *Matrix) IsValid() bool {
-	if len(*m) <= 0 || len((*m)[0]) <= 0 {
+// Shows if given row, col coordinates exist in Matrix m
+func (m Matrix) IsIndexValid(row, col int) bool {
+	if (len(m)) == 0 {
 		return false
 	}
-	numCols := len((*m)[0])
-	for _, row := range *m {
-		if len(row) != numCols {
-			return false
-		}
+
+	numRows, numCols := len(m), len((m)[0])
+
+	isRowValid := row >= 0 && row < numRows
+	isColValid := col >= 0 && col < numCols
+
+	if !isRowValid || !isColValid {
+		return false
 	}
 	return true
 }
-*/
+
+func (m Matrix) Print() {
+	for _, row := range m {
+		for _, num := range row {
+			fmt.Printf("%02d ", num)
+		}
+		fmt.Println()
+	}
+}
+
+// Multiplies 'adjNums' numbers in the same row or column, starting from index [rowStart][colStart]
+func (m *Matrix) ReturnProductOfAdjacentNumbers(rowOrCol rowOrCol, adjNums, rowStart, colStart int) (int, error) {
+	result := 1
+	isIterationFinished := false
+	currRow, currCol := rowStart, colStart
+	isCurrentIndexValid := m.IsIndexValid(rowStart, colStart)
+	if !isCurrentIndexValid {
+		return 0, errors.New(fmt.Sprintf("invalid starting index: [%v][%v]", rowStart, colStart))
+	}
+	if adjNums < 0 {
+		return 0, errors.New(fmt.Sprintf("invalid adjNums value: %v", adjNums))
+	}
+
+	switch rowOrCol {
+	case row:
+		for {
+			isIterationFinished = currCol > colStart+(adjNums-1)
+			isCurrentIndexValid = m.IsIndexValid(rowStart, currCol)
+			if !isCurrentIndexValid || isIterationFinished {
+				break
+			}
+			result *= (*m)[rowStart][currCol]
+			currCol++
+		}
+	case col:
+		for {
+			isIterationFinished = currRow > rowStart+(adjNums-1)
+			isCurrentIndexValid = m.IsIndexValid(currRow, colStart)
+			if !isCurrentIndexValid || isIterationFinished {
+				break
+			}
+			result *= (*m)[currRow][colStart]
+			currRow++
+		}
+	default:
+		return 0, errors.New(fmt.Sprintf("invalid 'rowOrCol' value: %v", rowOrCol))
+	}
+	return result, nil
+}
+
+func (m *Matrix) ReturnProductOfAdjacentNumbersDiag(leftOrRight leftOrRight, adjNums, rowStart, colStart int) (int, error) {
+	result := 1
+	isIterationFinished := false
+	currRow, currCol := rowStart, colStart
+	isCurrentIndexValid := m.IsIndexValid(rowStart, colStart)
+	if !isCurrentIndexValid {
+		return 0, errors.New(fmt.Sprintf("invalid starting index: [%v][%v]", rowStart, colStart))
+	}
+	if adjNums < 0 {
+		return 0, errors.New(fmt.Sprintf("invalid adjNums value: %v", adjNums))
+	}
+	switch leftOrRight {
+	case left:
+		for {
+			isIterationFinished = currCol == colStart-adjNums && currRow == rowStart+adjNums
+			isCurrentIndexValid = m.IsIndexValid(currRow, currCol)
+			if !isCurrentIndexValid || isIterationFinished {
+				break
+			}
+			result *= (*m)[currRow][currCol]
+			currCol--
+			currRow++
+		}
+	case right:
+		for {
+			isIterationFinished = currCol == colStart+adjNums && currRow == rowStart+adjNums
+			isCurrentIndexValid = m.IsIndexValid(currRow, currCol)
+			if !isCurrentIndexValid || isIterationFinished {
+				break
+			}
+			result *= (*m)[currRow][currCol]
+			currRow++
+			currCol++
+		}
+	default:
+		return 0, errors.New(fmt.Sprintf("invalid 'leftOrRight' value: %v", leftOrRight))
+	}
+	return result, nil
+}
---
+++ internal/entities/matrix_test.go
@@ -0,0 +1,188 @@
+package entities
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestResizeMatrix(t *testing.T) {
+	var m Matrix
+	var expected, actual []int
+	m.ResizeMatrix(2, 2)
+	expected, actual = []int{2, 2}, []int{len(m), len(m[0])}
+	assert.Equal(t, expected, actual)
+
+	m.ResizeMatrix(2, 5)
+	expected, actual = []int{2, 5}, []int{len(m), len(m[0])}
+	assert.Equal(t, expected, actual)
+
+	m.ResizeMatrix(5, 2)
+	expected, actual = []int{5, 2}, []int{len(m), len(m[0])}
+	assert.Equal(t, expected, actual)
+}
+
+func TestAssignValues(t *testing.T) {
+	var expected, m Matrix
+	m.ResizeMatrix(1, 4)
+	m.AssignValues([]int{4, 8, 15, 16, 23, 42})
+	expected = Matrix{{4, 8, 15, 16}}
+	assert.Equal(t, expected, m)
+
+	m.ResizeMatrix(1, 7)
+	m.AssignValues([]int{4, 8, 15, 16, 23, 42})
+	expected = Matrix{{4, 8, 15, 16, 23, 42, 0}}
+	assert.Equal(t, expected, m)
+
+	m.ResizeMatrix(4, 1)
+	m.AssignValues([]int{4, 8, 15, 16, 23, 42})
+	expected = make([][]int, 4);
+	expected[0] = []int{4}; expected[1] = []int{8}; expected[2] = []int{15}; expected[3] = []int{16}
+	assert.Equal(t, expected, m)
+}
+
+func TestIsIndexValid(t *testing.T) {
+	var emptyMatrix Matrix
+	assert.Equal(t, emptyMatrix.IsIndexValid(-1, -1), false)
+	assert.Equal(t, emptyMatrix.IsIndexValid(0, 0), false)
+	assert.Equal(t, emptyMatrix.IsIndexValid(1, 1), false)
+
+	matrix := Matrix{
+		{1, 2, 3},
+		{4, 5, 6},
+		{7, 8, 9},
+	}
+
+	assert.Equal(t, matrix.IsIndexValid(0, 0), true)
+	assert.Equal(t, matrix.IsIndexValid(1, 2), true)
+	assert.Equal(t, matrix.IsIndexValid(2, 1), true)
+	
+	assert.Equal(t, matrix.IsIndexValid(-1, -1), false)
+	assert.Equal(t, matrix.IsIndexValid(-1, 2), false)
+	assert.Equal(t, matrix.IsIndexValid(2, -1), false)
+	assert.Equal(t, matrix.IsIndexValid(2, 3), false)
+	assert.Equal(t, matrix.IsIndexValid(3, 2), false)
+	assert.Equal(t, matrix.IsIndexValid(8, 8), false)
+}
+
+func TestIsReturnProductOfAdjacentNumbers(t *testing.T) {
+	var m Matrix
+	m.ResizeMatrix(3, 3)
+	m.AssignValues([]int{4, 8, 15, 16, 23, 42, 2, 0, 1})
+
+	// First row, all 3 numbers
+	result, err := m.ReturnProductOfAdjacentNumbers("row", 3, 0, 0)
+	assert.Nil(t, err)
+	assert.Equal(t, 480, result)
+	// First row, last 2 numbers
+	result, err = m.ReturnProductOfAdjacentNumbers("row", 3, 0, 1)
+	assert.Nil(t, err)
+	assert.Equal(t, 120, result)
+	// First row, last number
+	result, err = m.ReturnProductOfAdjacentNumbers("row", 3, 0, 2)
+	assert.Nil(t, err)
+	assert.Equal(t, 15, result)
+	// Last two rows
+	result, err = m.ReturnProductOfAdjacentNumbers("row", 3, 1, 0)
+	assert.Nil(t, err)
+	assert.Equal(t, 15456, result)
+	result, err = m.ReturnProductOfAdjacentNumbers("row", 3, 2, 0)
+	assert.Nil(t, err)
+	assert.Equal(t, 0, result)
+
+	// First column, all 3 numbers
+	result, err = m.ReturnProductOfAdjacentNumbers("col", 3, 0, 0)
+	assert.Nil(t, err)
+	assert.Equal(t, 128, result)
+	// First column, last 2 numbers
+	result, err = m.ReturnProductOfAdjacentNumbers("col", 3, 1, 0)
+	assert.Nil(t, err)
+	assert.Equal(t, 32, result)
+	// First column, last 2 numbers
+	result, err = m.ReturnProductOfAdjacentNumbers("col", 3, 2, 0)
+	assert.Nil(t, err)
+	assert.Equal(t, 2, result)
+	// Last two columns
+	result, err = m.ReturnProductOfAdjacentNumbers("col", 3, 0, 1)
+	assert.Nil(t, err)
+	assert.Equal(t, 0, result)
+	result, err = m.ReturnProductOfAdjacentNumbers("col", 3, 0, 2)
+	assert.Nil(t, err)
+	assert.Equal(t, 630, result)
+
+	// First row, invalid column
+	result, err = m.ReturnProductOfAdjacentNumbers("row", 3, 0, 3)
+	assert.Equal(t, 0, result)
+	assert.EqualError(t, err, "invalid starting index: [0][3]")
+	// First column, invalid row
+	result, err = m.ReturnProductOfAdjacentNumbers("col", 3, 3, 0)
+	assert.Equal(t, 0, result)
+	assert.EqualError(t, err, "invalid starting index: [3][0]")
+	// Invalid adjNums value
+	result, err = m.ReturnProductOfAdjacentNumbers("col", -1, 0, 2)
+	assert.Equal(t, 0, result)
+	assert.EqualError(t, err, "invalid adjNums value: -1")
+	// Invalid rowOrCol values
+	result, err = m.ReturnProductOfAdjacentNumbers("invalid_value", 3, 0, 2)
+	assert.Equal(t, 0, result)
+	assert.EqualError(t, err, "invalid 'rowOrCol' value: invalid_value")
+}
+
+func TestIsReturnProductOfAdjacentNumbersDiag(t *testing.T) {
+	var m Matrix
+	m.ResizeMatrix(4, 4)
+	m.AssignValues([]int{4, 8, 15, 16, 23, 42, 2, 6, 1, 0, 7, 32, 57, 99, 25, 5})
+
+	// Left diagonals
+	result, err := m.ReturnProductOfAdjacentNumbersDiag("left", 2, 0, 3)
+	assert.Nil(t, err)
+	assert.Equal(t, 32, result)
+	result, err = m.ReturnProductOfAdjacentNumbersDiag("left", 3, 0, 3)
+	assert.Nil(t, err)
+	assert.Equal(t, 0, result)
+	result, err = m.ReturnProductOfAdjacentNumbersDiag("left", 3, 0, 2)
+	assert.Nil(t, err)
+	assert.Equal(t, 630, result)
+	result, err = m.ReturnProductOfAdjacentNumbersDiag("left", 2, 2, 3)
+	assert.Nil(t, err)
+	assert.Equal(t, 800, result)
+	result, err = m.ReturnProductOfAdjacentNumbersDiag("left", 1, 1, 3)
+	assert.Nil(t, err)
+	assert.Equal(t, 6, result)
+	result, err = m.ReturnProductOfAdjacentNumbersDiag("left", 3, 1, 3)
+	assert.Nil(t, err)
+	assert.Equal(t, 4158, result)
+	result, err = m.ReturnProductOfAdjacentNumbersDiag("left", 4, 0, 3)
+	assert.Nil(t, err)
+	assert.Equal(t, 0, result)
+
+	// Right diagonals
+	result, err = m.ReturnProductOfAdjacentNumbersDiag("right", 4, 0, 0)
+	assert.Nil(t, err)
+	assert.Equal(t, 5880, result)
+	result, err = m.ReturnProductOfAdjacentNumbersDiag("right", 4, 1, 1)
+	assert.Nil(t, err)
+	assert.Equal(t, 1470, result)
+	result, err = m.ReturnProductOfAdjacentNumbersDiag("right", 1, 1, 0)
+	assert.Nil(t, err)
+	assert.Equal(t, 23, result)
+	result, err = m.ReturnProductOfAdjacentNumbersDiag("right", 3, 1, 0)
+	assert.Nil(t, err)
+	assert.Equal(t, 0, result)
+
+	// Edge cases
+	result, err = m.ReturnProductOfAdjacentNumbersDiag("left", 99, 2, 3)
+	assert.Nil(t, err)
+	assert.Equal(t, 800, result)
+	result, err = m.ReturnProductOfAdjacentNumbersDiag("left", 2, -2, -3)
+	assert.Equal(t, 0, result)
+	assert.EqualError(t, err, "invalid starting index: [-2][-3]")
+	// Invalid rowOrCol values
+	result, err = m.ReturnProductOfAdjacentNumbersDiag("invalid_value", 3, 0, 2)
+	assert.Equal(t, 0, result)
+	assert.EqualError(t, err, "invalid 'leftOrRight' value: invalid_value")
+	// Invalid adjNums value
+	result, err = m.ReturnProductOfAdjacentNumbersDiag("left", -1, 0, 2)
+	assert.Equal(t, 0, result)
+	assert.EqualError(t, err, "invalid adjNums value: -1")
+}
