0d52b47e44d92e858269f09a0cdc435895fd3ed4
dbtrnl <daniel.bittar@alumni.usp.br>
2023-09-03 20:40:17 -0300

    feat: problem 19 solved

--- internal/problems/11-20.go
+++ internal/problems/11-20.go
@@ -4,6 +4,7 @@ import (
 	"math/big"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/dbtrnl/project-euler.go/internal/entities"
 	"github.com/dbtrnl/project-euler.go/internal/input_data"
@@ -533,5 +534,34 @@ func Problem18() int {
 		panic(err)
 	}
 	answer = triangle.FindMaximumSumPath()
+	return answer
+}
+
+// **Problem 19 - Counting Sundays**
+//
+// You are given the following information, but you may prefer to do some research for yourself.
+//
+// - 1 Jan 1900 was a Monday.
+//
+// - Thirty days has September, April, June and November.
+//
+// All the rest have thirty-one, Saving February alone,
+//
+// Which has twenty-eight, rain or shine. And on leap years, twenty-nine.
+//
+// - A leap year occurs on any year evenly divisible by 4, but not on a century unless it is divisible by 400.
+//
+// How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
+func Problem19() int {
+	start, end, answer := 1901, 2000, 0
+
+	for year := start; year <= end; year++ {
+		for month := 1; month <= 12; month++ {
+			date := time.Date(year, time.Month(month), 1, 0, 0, 0, 0, time.UTC)
+			if date.Weekday() == time.Sunday {
+				answer++
+			}
+		}
+	}
 	return answer
 }
